a3d57d479bdb5c6f23d6c0453e9aaed26c9b9781
sagar s.k <sks788056@gmail.com>
2025-11-25 14:24:13 +0545

    implement loginPage

--- frontend/src/Landing_Page/Login/LoginPage.jsx
+++ frontend/src/Landing_Page/Login/LoginPage.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
-    return ( <h1>Iam LoginPage</h1> );
+    const [form , setForm] = useState({email:"" , password:""});
+
+    const [showPassword , setShowPassword] = useState(false);
+
+    const handleChange = (e)=>{
+        setForm({...form , [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e)=>{
+
+    }
+    return (<div className='loginPage signupPage'> 
+    <div className='cardWrapper'>
+        <div className="card">
+        <h2 className="title">Login</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        {/* Email */}
+        <div className="input-box">
+          <i className="fa-solid fa-envelope icon"></i>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {/* Password */}
+        <div className="input-box">
+          <i className="fa-solid fa-lock icon"></i>
+
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
+
+          {/* Show / Hide Password */}
+          <i
+            className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} eye-icon`}
+            onClick={() => setShowPassword(!showPassword)}
+          ></i>
+        </div>
+
+        <button className="btn btn-form" type="submit">
+          Login
+        </button>
+
+      </form>
+
+    </div>
+    </div>
+    </div>    );
 }
 
 export default LoginPage;
