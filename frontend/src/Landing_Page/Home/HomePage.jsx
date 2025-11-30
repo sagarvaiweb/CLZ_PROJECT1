6fbf184e8a15adab1b482be9134c79ad598d05a3
sagar s.k <sks788056@gmail.com>
2025-11-30 06:35:00 +0545

    implement homePage

--- frontend/src/Landing_Page/Home/HomePage.jsx
+++ frontend/src/Landing_Page/Home/HomePage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { sampleListings } from '../../data';
 
 function HomePage() {
-    return ( <h1>Iam HomePage</h1> );
+    return ( <>
+    <ul className='cardWrap'>
+        {
+            sampleListings.map((listing , index)=>{       
+         return (
+        <li key={index}>
+
+        <a href="" className='text-decoration-none'>
+        <div className="card  card_top text-start  card-image-overlay " >
+        <img src={listing.image.url} className="card-img-top"   alt="listing_image"/>
+        <div className="card-body">
+        <h5 className="card-title">{listing.title}</h5>
+        <p className="card-text">{listing.price}/night</p>
+       
+        </div>
+        </div>
+        </a>
+               </li>)
+
+            })
+        }
+    </ul>
+    </>);
 }
 
 export default HomePage;
