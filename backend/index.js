339e3b0cd4afcc7ede630b2170effe85885ce632
sagar s.k <sks788056@gmail.com>
2025-11-30 18:33:25 +0545

    tested the first demo api

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,22 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000 ;
 const URL = process.env.MONGO_URL ;
 
-app.listen(PORT ,()=>{
-    console.log(`app is listening to the port ${PORT}`);
-    mongoose.connect(URL);
+mongoose.connect(URL).then(()=>{
     console.log("mongodb is connected");
-}) 
+    app.listen(PORT , ()=>{
+        console.log(`app is listening to the port ${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.error("mongodb connection is failed:",err);
+})
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hellow iam sagar");
+})
